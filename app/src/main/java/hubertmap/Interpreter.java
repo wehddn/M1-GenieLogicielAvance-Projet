cc917127c035f8c0c661a7980c0bb3ce39fe9639
LE JEUNE alban <albanlejeune94@gmail.com>
2023-03-09 15:32:55 +0100

    Creation of the Classes According to the UML Diagram + Parser (not tested)

---
+++ app/src/main/java/hubertmap/Interpreter.java
@@ -0,0 +1,20 @@
+import java.io. * ;
+import java.util.Scanner;
+
+public class Interpreter extends InterpreterV0 {
+    public static void main(String[] args) {
+        try {
+            File file = new File("test.csv");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] values = line.split(",");
+                Edge edge = new Edge(values[0], values[1], values[2]);
+                System.out.println(edge);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/hubertmap/InterpreterV0.java
@@ -0,0 +1,7 @@
+public abstract class InterpreterV0 {
+    
+    private String line;
+    private String TerminalStationDeparture;
+    private String TerminalStationArrival;
+    private (int,int) StartingTime;   
+}

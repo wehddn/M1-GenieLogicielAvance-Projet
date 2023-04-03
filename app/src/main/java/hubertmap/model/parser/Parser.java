4c3d6c8a96e3c5c7bd7e16749728e1793799b272
SONNEVILLE charly <charly.sonneville@gmail.com>
2023-04-03 10:16:57 +0200

    parser change and add class

--- app/src/main/java/hubertmap/model/parser/Parser.java
+++ app/src/main/java/hubertmap/model/parser/Parser.java
@@ -1,43 +1,74 @@
 package hubertmap.model.parser;
 
 import hubertmap.model.Time;
+import hubertmap.model.transport.EdgeTransport;
 import hubertmap.model.transport.Station;
 import java.io.*;
+import java.util.*;
 import java.util.Scanner;
 
 public class Parser extends ParserFactory {
     // StartingStation; StartingStationLatitude; StartingStationLongitude; EndingStation;
     // EndingStationLatitude; EndingStationLongitude; Line; Time; Distance;
     /** Parses the input CSV file and returns the network data as a tuple of Stations and Edges. */
+    private List<Station> list = new ArrayList<>();
+
     public Parser() {
         try {
-            File file = new File("src/main/java/hubertmap/model/map_data.csv");
-            Scanner scanner = new Scanner(file);
-
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] values = line.split(";");
-
-                String station1Name = values[0].trim();
-                float station1Lat = Float.parseFloat(values[1].trim().split(",")[0]);
-                float station1Lon = Float.parseFloat(values[1].trim().split(",")[1]);
-                String station2Name = values[2].trim();
-                float station2Lat = Float.parseFloat(values[3].trim().split(",")[0]);
-                float station2Lon = Float.parseFloat(values[3].trim().split(",")[1]);
-                String lineName = values[4].trim();
-                String timeString = values[5].trim();
-                Time time =
-                        new Time(
-                                Integer.parseInt(timeString.split(":")[0]),
-                                Integer.parseInt(timeString.split(":")[1]));
-                float distance = Float.parseFloat(values[6].trim());
-                Station station1 = new Station(station1Name, lineName, station1Lat, station1Lon);
-                Station station2 = new Station(station2Name, lineName, station2Lat, station2Lon);
-            }
-
-            scanner.close();
-        } catch (FileNotFoundException e) {
+            parseCsv(openFile("src/main/java/hubertmap/model/map_data.csv"));
+        } catch (Exception e) {
             System.out.println("Le fichier n'a pas été trouvé : " + e.getMessage());
         }
+    }
+
+    public File openFile(String path) {
+        try {
+            File file = new File(path);
+            return file;
+        } catch (Exception e) {
+            System.out.println("Le fichier n'a pas été trouvé : " + e.getMessage());
+            return null;
+        }
+    }
+
+    public boolean stationAlreadyExist(String name, String line) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getName().equals(name) && list.get(i).getAllLines().contains(line)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void parseCsv(File file) throws Exception {
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] values = line.split(";");
+
+            String station1Name = values[0].trim();
+            float station1Lat = Float.parseFloat(values[1].trim().split(",")[0]);
+            float station1Lon = Float.parseFloat(values[1].trim().split(",")[1]);
+            String station2Name = values[2].trim();
+            float station2Lat = Float.parseFloat(values[3].trim().split(",")[0]);
+            float station2Lon = Float.parseFloat(values[3].trim().split(",")[1]);
+            String lineName = values[4].trim();
+            String timeString = values[5].trim();
+            Time time =
+                    new Time(
+                            Integer.parseInt(timeString.split(":")[0]),
+                            Integer.parseInt(timeString.split(":")[1]));
+            float distance = Float.parseFloat(values[6].trim());
+            if (!stationAlreadyExist(station1Name, lineName)
+                    && !stationAlreadyExist(station2Name, lineName)) {
+                Station station1 = new Station(station1Name, lineName, station1Lat, station1Lon);
+                list.add(station1);
+                Station station2 = new Station(station2Name, lineName, station2Lat, station2Lon);
+                list.add(station2);
+                EdgeTransport edge = new EdgeTransport(station1, station2, time, distance);
+                System.out.println("Created edge: " + edge);
+            }
+        }
+        scanner.close();
     }
 }
---
+++ app/src/main/java/hubertmap/model/transport/EdgeTransport.java
@@ -0,0 +1,57 @@
+package hubertmap.model.transport;
+
+import hubertmap.model.Time;
+
+public class EdgeTransport {
+
+    private Time time;
+    private Float Distance;
+    private Station StartingStation;
+    private Station EndingStation;
+
+    public EdgeTransport(Station StartingStation, Station EndingStation) {
+        this.StartingStation = StartingStation;
+        this.EndingStation = EndingStation;
+    }
+
+    public EdgeTransport(
+            Time time, Float Distance, Station StartingStation, Station EndingStation) {
+        this.time = time;
+        this.Distance = Distance;
+        this.StartingStation = StartingStation;
+        this.EndingStation = EndingStation;
+    }
+
+    public EdgeTransport(
+            Station StartingStation, Station EndingStation, Time time, Float Distance) {
+        this.StartingStation = StartingStation;
+        this.EndingStation = EndingStation;
+        this.time = time;
+        this.Distance = Distance;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return StartingStation
+                + " - "
+                + EndingStation
+                + "; time : "
+                + time
+                + "; distance : "
+                + Distance;
+    }
+
+    /*public Edge(Station StartingStation, Float StartingStationLatitude, Float StartingStationLongitude, Station EndingStation, Float EndingStationLatitude, Float EndingStationLongitude,  String Line, String Time, Float Distance){
+        this.StartingStation = StartingStation;
+        this.StartingStationLatitude = StartingStationLatitude;
+        this.StartingStationLongitude = StartingStationLongitude;
+        this.EndingStation = EndingStation;
+        this.EndingStationLatitude = EndingStationLatitude;
+        this.EndingStationLongitude = EndingStationLongitude;
+        this.Line = Line;
+        this.Time = Time;
+        this.Distance = Distance;
+    }*/
+
+}

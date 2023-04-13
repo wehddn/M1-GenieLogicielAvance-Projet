a037d586ce2a7bb1bbcb60b4659f3e74c4b4a432
Paul Gangneux <paul.v.gangneux@gmail.com>
2023-04-14 00:08:16 +0200

    added tests for shortest path

--- app/src/main/java/hubertmap/model/transport/Network.java
+++ app/src/main/java/hubertmap/model/transport/Network.java
@@ -3,6 +3,7 @@ package hubertmap.model.transport;
 import edu.uci.ics.jung.algorithms.shortestpath.DijkstraShortestPath;
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.SparseGraph;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
@@ -19,7 +20,7 @@ public class Network {
 
     DijkstraShortestPath distancePaths;
 
-    public Network() {
+    public Network(Collection<EdgeTransport> edges) {
         graph = new SparseGraph<>();
         stations = new HashMap<>();
         minimumLongitude = 180;
@@ -27,12 +28,22 @@ public class Network {
         minimumLatitude = 90;
         maximumLatitude = -90;
 
+        if (edges != null) {
+            for (EdgeTransport e : edges) {
+                addEdge(e);
+            }
+        }
+
         distancePaths =
                 new DijkstraShortestPath(
                         graph,
                         (Object e) -> {
                             return ((EdgeTransport) e).getDistance();
                         });
+    }
+
+    public Network() {
+        this(null);
     }
 
     public void addEdge(EdgeTransport edge, Station station1, Station station2) {
@@ -52,6 +63,10 @@ public class Network {
 
         stations.putIfAbsent(station1.getName(), station1);
         stations.putIfAbsent(station2.getName(), station2);
+    }
+
+    public void addEdge(EdgeTransport edge) {
+        addEdge(edge, edge.getStartingStation(), edge.getEndingStation());
     }
 
     public Graph<Station, EdgeTransport> getGraph() {
@@ -74,10 +89,26 @@ public class Network {
         return maximumLongitude;
     }
 
+    /**
+     * Calculates the shortest path from station1 to station2 using
+     * the distance between two stations as weight
+     * 
+     * @param station1 starting station
+     * @param station2 destination station
+     * @return a list edges to visit in the correct order
+     */
     public List<EdgeTransport> shortestPath(Station station1, Station station2) {
         return distancePaths.getPath(station1, station2);
     }
 
+    /**
+     * Calculates the shortest path from station1 to station2 using
+     * the distance between two stations as weight
+     * 
+     * @param station1 name of the starting station
+     * @param station2 name of the destination station
+     * @return a list edges to visit in the correct order
+     */
     public List<EdgeTransport> shortestPath(String station1, String station2) {
         return shortestPath(stations.get(station1), stations.get(station2));
     }
